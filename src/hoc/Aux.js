a924aab8bfc20a6146f136f4bd2734d405621c69
nelma <nelmasb@gmail.com>
2018-06-03 06:55:41 -0300

    Aplicacao teste

---
+++ src/hoc/Aux.js
@@ -0,0 +1,5 @@
+//nao precida importar o react pq nao estamos usando JXS
+//esse arquivo é usado como auxiliar para que outros arquivos nao precisem envolver o JXS em divs
+
+const aux = ( props ) => props.children;
+export default aux;
