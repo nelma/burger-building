8e52a1bc4fb30b2569aaef079a157b31bfcaa947
Nelma Barros <nbarros@uolinc.com>
2018-06-27 17:03:26 -0300

    componente button

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,6 +21,9 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p>Continue to checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   )
 };
