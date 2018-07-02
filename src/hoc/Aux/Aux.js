6596fd52eadef412db533e284e010c9ae1eedc45
Nelma Barros <nbarros@uolinc.com>
2018-07-02 11:37:07 -0300

    reordenando Layout para dentro do hoc

---
+++ src/hoc/Aux/Aux.js
@@ -0,0 +1,5 @@
+//nao precida importar o react pq nao estamos usando JSX
+//esse arquivo é usado como auxiliar para que outros arquivos nao precisem envolver o JSX em divs
+
+const aux = ( props ) => props.children;
+export default aux;
