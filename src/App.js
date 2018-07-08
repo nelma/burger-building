4f3c22e12c6bd722b5daf7592ee8652a988f641b
nelma <nelmasb@gmail.com>
2018-07-08 13:01:39 -0300

    criando um projeto no firebase e fazendo o post com axios

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+/* import 'flatpickr/dist/themes/material_green.css'; */
+
+import Flatpickr from 'react-flatpickr';
+
 class App extends Component {
+  
+  constructor() {
+    super();
+ 
+    this.state = {
+      date: new Date()
+    };
+  }
+
+
   render() {
+    const { date } = this.state;
+
     return (
       <div>
         <Layout>
           <BurgerBuilder />
-        </Layout>
+          <Flatpickr data-enable-time
+              value={date}
+              onChange={date => { this.setState({date}) }} />
+              </Layout>
       </div>
     );
   }
