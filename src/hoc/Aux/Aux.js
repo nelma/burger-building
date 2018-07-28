da787a1450d87d21926533ccdcc8acb76c715586
nelma <nelmasb@gmail.com>
2018-07-28 09:01:13 -0300

    seção 13 - orders

--- src/hoc/Aux/Aux.js
+++ src/hoc/Aux/Aux.js
@@ -1,5 +1,3 @@
-//nao precida importar o react pq nao estamos usando JSX
-//esse arquivo é usado como auxiliar para que outros arquivos nao precisem envolver o JSX em divs
+const aux = (props) => props.children;
 
-const aux = ( props ) => props.children;
 export default aux;
