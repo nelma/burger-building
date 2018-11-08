7612e2d975f74972626204d52489480c6c3590e6
Nelma Barros <nbarros@uolinc.com>
2018-11-08 17:03:11 -0200

    adicionando token e logout

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
 
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import buguerBuilderReducer from './store/reducers/burguerBuilder';
-import orderReducer from './store/reducers/order';
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import buguerBuilderReducer from "./store/reducers/burguerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducers from "./store/reducers/auth";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    burgerBuilder: buguerBuilderReducer,
-    order: orderReducer
+  burgerBuilder: buguerBuilderReducer,
+  order: orderReducer,
+  auth: authReducers
 });
 
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
-
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,49 +1,68 @@
-import axios from 'axios';
+import axios from "axios";
 
-import * as actionTypes from './actionsTypes';
+import * as actionTypes from "./actionsTypes";
 
 export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START
-    };
+  return {
+    type: actionTypes.AUTH_START
+  };
 };
 
-export const authSuccess = (authData) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        authData: authData
-    };
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId
+  };
 };
 
-export const authFail = (error) => {
-    return {
-        type: actionTypes.AUTH_FAIL,
-        error: error
-    };
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
 };
 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
+  return dispatch => {
+    dispatch(authStart());
 
-        const authData = {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB8rEXDN2YjeTneTrKX0wWx4pMnen9sJBc';
-        if(!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB8rEXDN2YjeTneTrKX0wWx4pMnen9sJBc';
-        }
-        axios.post(url, authData)
-            .then(response => {
-                console.log(response);
-                dispatch(authSuccess(response.data));
-            })
-            .catch(error => {
-                console.log(error);
-                dispatch(authFail(error));
-            })
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
     };
+
+    let url =
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB8rEXDN2YjeTneTrKX0wWx4pMnen9sJBc";
+    if (!isSignup) {
+      url =
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB8rEXDN2YjeTneTrKX0wWx4pMnen9sJBc";
+    }
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err.response.data.error));
+      });
+  };
 };
